ac3fd15431d5dfea407fcf36497efd79847e4f07
miho s <>
2026-05-30 08:34:34 +0900

    developped 20260529

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-//tokusho-btn
-$(function() {
-    $(".tokusho_btn").click(function() {
-        $(".tokusho_text").toggleClass("is-active");
-    });
-});
-
 // header_fetch_target
 fetch('header.html')
     .then(response => response.text())
